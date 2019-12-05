4f0ab7010d951cd32a9b9e3af35360741bc9da8a
MaxWillekes <max.willekes@student.hku.nl>
2019-12-05 11:59:59 +0100

    YEET BITCH

--- XInputMario/Assets/Scripts/Truck.cs
+++ XInputMario/Assets/Scripts/Truck.cs
@@ -49,6 +49,9 @@ public class Truck : MonoBehaviour
         //PlayExplosion add an awesome EXPLOSION PARTICLE EFFECT to the game
         Pool.Instance.ReturnChild();
 
+        if (collision.gameObject.tag == "Obstacle")
+            Destroy(collision.gameObject);
+
         explosion.transform.position = collision.transform.position;
         explosion.gameObject.SetActive(true);
         explosion.Play();
